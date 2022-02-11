ff91d463b471b49702d545a02d730ed0adcd8914
Troy Fluegge <troy@hashicorp.com>
2022-02-11 15:44:07 -0600

    SG Update test

--- main.tf
+++ main.tf
@@ -54,6 +54,13 @@ resource "aws_security_group" "hashicat" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+    ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port       = 0
